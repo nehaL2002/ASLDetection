d3d601c1852a2f4465fb37196067aaa33a6a1397
nehaL2002 <32632268+nehaL2002@users.noreply.github.com>
2025-03-08 14:04:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,35 +7,21 @@ from PIL import Image
 
 model = YOLO('aslmodel.pt')
 
-def predict_image(img, conf_threshold, iou_threshold):
-    """Predicts objects in an image using a YOLOv8 model with adjustable confidence and IOU thresholds."""
-    results = model.predict(
-        source=img,
-        conf=conf_threshold,
-        iou=iou_threshold,
-        show_labels=True,
-        show_conf=True,
-        imgsz=800,
-    )
+def detection_image(image):
+    results = model.predict(source=image, conf=0.25)
+    for result in results:
+        img = result.plot()  # Get the annotated image (numpy array)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+        pil_img = Image.fromarray(img_rgb)  # Convert to PIL image
+        return pil_img
 
-    for r in results:
-        im_array = r.plot()
-        im = Image.fromarray(im_array[..., ::-1])
-
-    return im
-
-iface = gr.Interface(
-    fn=predict_image,
-    inputs=[
-        gr.Image(sources="webcam", type="pil", label="Capture Image"),
-        gr.Slider(minimum=0, maximum=1, value=0.25, label="Confidence threshold"),
-        gr.Slider(minimum=0, maximum=1, value=0.45, label="IoU threshold"),
-    ],
-    outputs=gr.Image(type="pil", label="Result"),
-    live=True,  # Enables real-time processing
-    title="Ultralytics Gradio",
-    description="Capture images from your webcam for real-time inference using the Ultralytics YOLOv8n model.",
+demo = gr.Interface(
+    fn=detection_image,
+    inputs=gr.Image(type="filepath"),
+    outputs=gr.Image(),
+    title="ASL Alphabetic Letter Detection",
+    description="_Using YOLOv8_"
 )
 
 if __name__ == "__main__":
-    iface.launch()
+    demo.launch()
